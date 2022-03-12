8113bab5105a4530dd3dde2fd3a5a1681b372f44
AlanCastellanosSulub <adjcastellanos@gmail.com>
2022-03-12 13:56:21 -0600

    6.1 - ¿Qué es un servicio y cómo generarlo?

--- src/app/modules/task/components/card/card.component.ts
+++ src/app/modules/task/components/card/card.component.ts
@@ -1,38 +1,26 @@
 import { AfterContentInit, AfterViewChecked, Component, DoCheck, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChildren } from '@angular/core';
+import { TestCourseService } from '@shared/services/test-course.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit, DoCheck, OnChanges,
-AfterViewChecked, AfterContentInit {
+export class CardComponent implements OnInit{
   @ViewChildren('refId') elemantRefId:ElementRef=new ElementRef('');
   @Input() idOrder:string | number = 0;
   @Input() items:Array<any> = [];
 
-  constructor() { }
-  
-  ngAfterContentInit(): void {
-    
-  }
+  constructor(private testCourseService:TestCourseService) { }
 
   ngOnInit(): void {
     //console.log(this.items)
     
   }
 
-  ngDoCheck(): void {
-    
-  }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    //console.log(changes);
-    
-  }
-
-  ngAfterViewChecked(): void {
-    
+  sendData():void{
+    this.testCourseService.setData('Hola desde card 😉')
   }
 
 }
---
+++ src/app/shared/services/test-course.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestCourseService {
+
+  public dataTest:string='Hola Mundo';
+
+  constructor() { }
+
+  setData(msg:string):void{
+    this.dataTest=msg;
+  }
+
+}
